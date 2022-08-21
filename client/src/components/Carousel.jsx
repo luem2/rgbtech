7544b4bf0ef54fe2828bcd1259fef331a626bf8c
lvlaciel <96140694+lvlaciel@users.noreply.github.com>
2022-08-21 11:23:34 -0300

    Update(client/src): Carousel-Product

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -6,7 +6,7 @@ import { IoMdArrowDropleft, IoMdArrowDropright } from 'react-icons/io';
 import logo from "../assets/logo-dibujo2.png"
 
 const imagnaryProducts = [
-	// logo,
+	 logo,
     "https://www8.hp.com/my/en/images/02_hero_r1_700x500_tcm193_2638293_tcm193_2638346_tcm193-2638293.png",
     "https://th.bing.com/th/id/R.4896c93d881bb34581dc88f7f3201e88?rik=VDJiYq0VLTKefg&pid=ImgRaw&r=0",
     "https://th.bing.com/th/id/R.5de1e61a3db24af4bd4ad7dedc8da998?rik=Bi2LQWTh3yxseQ&pid=ImgRaw&r=0",
@@ -20,23 +20,20 @@ function Carousel() {
 
 	const slideRef = useRef();
 
-    const removeAnimation = ()=> {
-        slideRef.current.classList.remove("fade-anim")
-    };
-   
+	const removeAnimation = () => {
+		slideRef.current.classList.remove("fade-anim");
+	};
+
 	useEffect(() => {
-        slideRef.current.addEventListener
-        ("animationend", removeAnimation)
-        slideRef.current.addEventListener
-        ("mouseenter", pauseSlider)
-        slideRef.current.addEventListener
-        ("mouseleave", startSlider)
-		
-        startSlider();
-        return () => {
-            pauseSlider();
-        };
-    }, []);
+		slideRef.current.addEventListener("animationend", removeAnimation);
+		slideRef.current.addEventListener("mouseenter", pauseSlider);
+		slideRef.current.addEventListener("mouseleave", startSlider);
+
+		startSlider();
+		return () => {
+			pauseSlider();
+		};
+	}, []);
 
 	const startSlider = () => {
 		slideInterval = setInterval(() => {
@@ -44,42 +41,46 @@ function Carousel() {
 		}, 3000);
 	};
 
-    const pauseSlider = ()=> {
-        clearInterval(slideInterval);
-    };
+	const pauseSlider = () => {
+		clearInterval(slideInterval);
+	};
 
 	const handleOnNextClick = () => {
 		count = (count + 1) % imagnaryProducts.length;
 		setCurrentIndex(count);
-        slideRef.current.classList.add("fade-anim")
+		slideRef.current.classList.add("fade-anim");
 	};
 	const handleOnPrevClick = () => {
 		const imgsLength = imagnaryProducts.length;
 		count = (currentIndex + imgsLength - 1) % imgsLength;
 		setCurrentIndex(count);
-        slideRef.current.classList.add("fade-anim")
+		slideRef.current.classList.add("fade-anim");
 	};
 
 	return (
-		<div ref={slideRef} className="w-auto select-none cursor-pointer">
+		<div ref={slideRef} className="w-auto select-none">
 			<div className="flex flex-wrap justify-center">
-				<img className="pt-5 pb-20 hover:scale-105 transition duration-300 ease-in-out" src={imagnaryProducts[currentIndex]} alt="" />
+				<img
+					className="pt-5 pb-20 cursor-pointer hover:scale-105 transition duration-300 ease-in-out"
+					src={imagnaryProducts[currentIndex]}
+					alt=""
+				/>
 			</div>
 
-			<div
-				className="fixed w-full top-1/2 transform -translate-y-1/2 
-            px-3 flex justify-between items-center"
-			>
-				<button className="absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
-                 
-                onClick={handleOnPrevClick}><IoMdArrowDropleft size={40}/>
-                </button>
-				<button className="absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
-         
-                onClick={handleOnNextClick}><IoMdArrowDropright size={40}/>
-                </button>
+			<div className="flex flex-wrap justify-center">
+				<button
+					className="absolute bottom-80 flex items-center justify-center text-center left-0"
+					onClick={handleOnPrevClick}
+				>
+					<IoMdArrowDropleft size={40} />
+				</button>
+				<button
+					className="absolute bottom-80 flex items-center justify-center text-center right-0"
+					onClick={handleOnNextClick}
+				>
+					<IoMdArrowDropright size={40} />
+				</button>
 			</div>
-			
 		</div>
 	);
 }
--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -1,21 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { AiOutlineHeart } from "react-icons/ai";
+import { GiShoppingCart } from "react-icons/gi";
 
 function Product({ id, name, price, img }) {
 	return (
-		<div className="flex justify-center p-4">
-			<div className="rounded-lg shadow-lg bg-white max-w-sm">
-				<img className="rounded-t-lg " src={img} alt={name} />
+		<div className="flex flex-wrap justify-center p-10">
+			<div className="flex flex-wrap justify-center rounded-lg shadow-xl max-w-sm">
+				<img className="shadow-xl hover:opacity-60 transition duration-300 ease-in-out bg-pink-700" 
+				src={img} 
+				alt={name} />
 
-				<div className="p-6">
+				<div className="flex flex-col justify-between p-6">
 					<h5 className="text-gray-900 text-xl font-medium mb-2">{name}</h5>
 					<p className="text-gray-700 text-base mb-4">${price}</p>
-					<Link to={`/productDetails/${id}`}>
-						<button
-							type="button"
-							className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-						>
-							Details
+					<Link className="flex flex-row bg-pink justify-between gap-5" to={`/productDetails/${id}`}>
+						<button>
+						<GiShoppingCart className="bg-white-600 rounded-2xl py-0.5 p-1 w-8 h-8 hover:scale-110 "/>
+						</button>
+						<button>
+						<AiOutlineHeart className="bg-pink-600 rounded-2xl py-0.5 p-1 w-7 h-7 hover:scale-110"/>
 						</button>
 					</Link>
 				</div>
