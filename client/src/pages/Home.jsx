185d12408ec0c57d0763f5ea33ffe29583fb8fa4
Luem <luchemma@gmail.com>
2022-08-21 00:47:15 -0300

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getAllProducts } from "../store/slices/products/thunks.js";
 import Carousel from "../components/Carousel.jsx";
 import Header from "../components/Header/Header.jsx";
-import Products from "../components/Products.jsx";
 import CategoriesCarousel from "../components/CategoriesSection/CategoriesSection.jsx";
 import Footer from "../components/Footer.jsx";
 
 const Home = () => {
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(getAllProducts());
+	}, []);
+
 	return (
 		<div className="min-h-screen grid">
 			<Header />
