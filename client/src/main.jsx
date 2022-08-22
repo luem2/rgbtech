e9e00fe67c40ad3ee332a59f512d4da3385b1fb3
lvlaciel <96140694+lvlaciel@users.noreply.github.com>
2022-08-22 10:29:08 -0300

    Front/ SimpleChanges

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,8 +10,8 @@ axios.defaults.baseURL = "http://localhost:3003/";
 
 createRoot(document.getElementById("root")).render(
 	<Provider store={store}>
-		<React.StrictMode>
+		
 			<App />
-		</React.StrictMode>
+		
 	</Provider>
 );
