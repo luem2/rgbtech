894c9ab850bfcc0f20ed0ed5a353a0b77e936559
Esteban0010 <estebanpilchuman02@gamil.com>
2022-08-28 15:44:02 -0300

    a

--- api/src/routes/User.js
+++ api/src/routes/User.js
@@ -67,7 +67,7 @@ router.get("/profile/:id", validateToken, async (req, res) => {
 	try {
 		const { id } = req.params;
 
-		const user = await User.findByPk({ id })
+		const user = await User.findByPk( id )
 		console.log(user,"User encontrado")
 		if (!Object.keys(user).length) {
 			res.sendStatus(404)
