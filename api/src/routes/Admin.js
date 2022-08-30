c74ca8864b55e7d252041972d5c4ff4e260bf61a
JSebastian101 <jhonsp101@gmail.com>
2022-08-30 15:15:11 -0500

    update(routes/admin) rutas para el dashboard de administrador

--- api/src/routes/Admin.js
+++ api/src/routes/Admin.js
@@ -1,14 +1,42 @@
 const { Router } = require("express");
-const { User, Venta, Products, Brand, Tag } = require("../db");
+const { User, Sale, Product, Brand, Tag } = require("../db");
 const jwt = require("jsonwebtoken");
 const { Op } = require("sequelize");
 const {validateToken} = require("../middlewares/userMiddleware.js");
 
 const router = Router();
 
+/* 
+users
+sales
+stock
+
+*/
+
+
 
 router.get('/dashboard', validateToken, async(req, res) => {
-
+  //Para el historial de compras
+  let sales = await Sale.findAll({
+    include: [
+      {
+        model: Tag,
+        through: { attributes: [] },
+      },
+      {
+        model: Product,
+        through: { attributes: [] },
+      },
+      {
+        model: User,
+        through: { attributes: [] },
+      },
+      {
+        model: Brand,
+        through: { attributes: [] },
+      },
+    ],
+  });
 })
 
 
