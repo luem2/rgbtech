644bea74589095a424ce7b441df06c35c81d6fe5
H-Bauty-C <hugoceci2003@gmail.com>
2022-09-05 11:06:48 -0300

    añadir product a awards

--- api/src/Utils/awards.js
+++ api/src/Utils/awards.js
@@ -34,12 +34,61 @@ const Products = [
       NoiseCancellation: "Yes",
       microphone: "Yes",
       batteryLife: "5 hs",
-      waterproof: "Yes",
+      waterproof: "Yes"
     }
     ],
     img: "https://http2.mlstatic.com/D_NQ_NP_745358-MLA45449928637_042021-O.webp",
     stock: 200,
   },
+  {
+  name: "beanie with bluetooth",
+  points: 99,
+  description: "gift for men, women, boys, girls, teens, teenagers, V5.0 Bluetooth, unique and cool winter hat with headphones and speakers",
+  specifications: [{connection: "Bluetooth", 
+   batteryLife: "10 hs",
+   microphone: "Yes",
+   washable:"Yes"}
+],
+  img: "https://m.media-amazon.com/images/I/8130jrwWWWL._AC_UX522_.jpg",
+  stock: 543,
+  freeShipping: true
+},
+  {
+  name: "headphone stand",
+  points: 25,
+  description: "Universal under desk design clamps to desks up to 37mm thick and as narrow as 7mm for a near universal fit on most desks. Spring clip is easy to remove and reattach",
+  specifications: [{color: "Black", 
+   compatibleDevices: "Headphones",
+   material: "Rubber Glass Polycarbonate Wood"}
+],
+  img: "https://m.media-amazon.com/images/I/616qQCQHs2L._AC_SX425_.jpg",
+  stock: 342,
+  freeShipping: true
+},
+  {
+  name: "SABRENT Hub USB-C 3.0",
+  points: 25,
+  description: "Perfect to add an SD card reader and regular USB ports. All ports they're fully functional. The bottom of the hub is magnetic to hold it in place.",
+  specifications: [{color: "Gray", 
+   USB: "3.0",
+   dualSlot: "SD y TF + 3 hubs USB"}
+],
+  img: "https://images-na.ssl-images-amazon.com/images/I/81GUMFyIxqL.__AC_SX300_SY300_QL70_FMwebp_.jpg",
+  stock: 168,
+  freeShipping: true
+},
+  {
+  name: "led strip lights",
+  points: 19,
+  description: "Premium 5050 LED Lights: Our light strips are made of high quality bright 5050 LED lights. It is not waterproof and is designed for indoor use. It can decorate your bedroom, ceiling, stairs, kitchen cabinet, porch, desk, and living room, especially great for holidays like Christmas, Halloween, parties, etc.",
+  specifications: [{color: "Multicolored", 
+   LightSourceType: "LED",
+   PowerSource: "Corded Electric"}
+],
+  img: "https://cdn.shopify.com/s/files/1/0599/5262/7880/products/1_7f9002a1-0e33-494d-80b7-55cde90cf9da_700x700.jpg?v=1636148746",
+  stock: 93,
+  freeShipping: true
+}
 ]
 
 module.exports = {Products}
