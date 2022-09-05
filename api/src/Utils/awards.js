a2689198893dac427d4eb3807136e0e3bb9d6efc
H-Bauty-C <hugoceci2003@gmail.com>
2022-09-05 11:42:46 -0300

    modificacion de Utils/awards

--- api/src/Utils/awards.js
+++ api/src/Utils/awards.js
@@ -1,8 +1,8 @@
 
 const Products = [
   {
-    name: "Soporte apoya celular",
-    points: 7.34,
+    name: "Support supports cell phone",
+    points: 200,
     description: "Extensible y flexible, lo mejor solo para vos",
     specifications: [{
       "length": "26cm-36cm",
@@ -13,8 +13,8 @@ const Products = [
     stock: 0,
   },
   {
-    name: "Aro de luz led",
-    points: 27,
+    name: "led ring light",
+    points: 650,
     description: "No pierda la oportunidad el mejor aro de luz solo para vos",
     specifications: [{
       "connector": "USB",
@@ -26,8 +26,8 @@ const Products = [
     stock: 200,
   },
   {
-    name: "Auriculares in-ear inalámbricos F9-5",
-    points: 27,
+    name: "Headphones in-ear wireless F9-5",
+    points: 800,
     description: "l formato perfecto para vos. Al ser in-ear, mejoran la calidad del audio y son de tamaño pequeño para poder insertarse en tu oreja. Son ideales para acompañarte a la hora de hacer ejercicio mientras te sumergís en el mejor sonido envolvente.",
     specifications: [{
       wireless: "Yes",
@@ -42,7 +42,7 @@ const Products = [
   },
   {
   name: "beanie with bluetooth",
-  points: 99,
+  points: 1000,
   description: "gift for men, women, boys, girls, teens, teenagers, V5.0 Bluetooth, unique and cool winter hat with headphones and speakers",
   specifications: [{connection: "Bluetooth", 
    batteryLife: "10 hs",
@@ -55,7 +55,7 @@ const Products = [
 },
   {
   name: "headphone stand",
-  points: 25,
+  points: 500,
   description: "Universal under desk design clamps to desks up to 37mm thick and as narrow as 7mm for a near universal fit on most desks. Spring clip is easy to remove and reattach",
   specifications: [{color: "Black", 
    compatibleDevices: "Headphones",
@@ -67,7 +67,7 @@ const Products = [
 },
   {
   name: "SABRENT Hub USB-C 3.0",
-  points: 25,
+  points: 450,
   description: "Perfect to add an SD card reader and regular USB ports. All ports they're fully functional. The bottom of the hub is magnetic to hold it in place.",
   specifications: [{color: "Gray", 
    USB: "3.0",
@@ -79,7 +79,7 @@ const Products = [
 },
   {
   name: "led strip lights",
-  points: 19,
+  points: 350,
   description: "Premium 5050 LED Lights: Our light strips are made of high quality bright 5050 LED lights. It is not waterproof and is designed for indoor use. It can decorate your bedroom, ceiling, stairs, kitchen cabinet, porch, desk, and living room, especially great for holidays like Christmas, Halloween, parties, etc.",
   specifications: [{color: "Multicolored", 
    LightSourceType: "LED",
