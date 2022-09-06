6b5277b024553dd3ade339c6fe670cde00b9a2cc
Luem <luchemma@gmail.com>
2022-09-06 14:51:46 -0300

    Update app.js

--- api/src/app.js
+++ api/src/app.js
@@ -3,19 +3,19 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const routes = require("./routes/index.js");
-const path = require("path")
+const path = require("path");
 require("./db.js");
 
 const server = express();
 server.use(express.urlencoded({ extended: false }));
-server.use(express.static(path.join(__dirname, "public")))
+server.use(express.static(path.join(__dirname, "public")));
 server.name = "API";
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5173"); // update to match the domain you will make the request from
+	res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // update to match the domain you will make the request from
 	res.header("Access-Control-Allow-Credentials", "true");
 	res.header(
 		"Access-Control-Allow-Headers",
