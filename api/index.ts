5afc259af4ba40f3285984f8a2dd3791cf74d0f8
Luem <luchemma@gmail.com>
2023-03-05 22:11:23 -0300

    update files and package json

--- api/index.ts
+++ api/index.ts
@@ -17,21 +17,22 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
-const createAllProducts = require('./src/Utils/createProducts.js')
-const addBrands = require('./src/Utils/createBrands.js')
-const addTags = require('./src/Utils/createTags.js')
-const createUsers = require('./src/Utils/createUsers.js')
-const createAllProductsOfAwards  = require("./src/Utils/createAwards")
+import server from './src/app.js'
+import { conn } from './src/db.js'
+import createAllProducts from './src/Utils/createProducts.js'
+import addBrands from './src/Utils/createBrands.js'
+import addTags from './src/Utils/createTags.js'
+import createUsers from './src/Utils/createUsers.js'
+import createAllProductsOfAwards from './src/Utils/createAwards'
+
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  addTags()
-  addBrands()
-  createAllProducts();
-  createUsers();
-  createAllProductsOfAwards()
-  server.listen(3003, () => {
-    console.log('%s listening at 3003'); // eslint-disable-line no-console
-  });
-});
+    addTags()
+    addBrands()
+    createAllProducts()
+    createUsers()
+    createAllProductsOfAwards()
+    server.listen(3003, () => {
+        console.log('%s listening at 3003') // eslint-disable-line no-console
+    })
+})
