af6b53aa74841272f2852d68c489ab000df7f5b3
Luem <luchemma@gmail.com>
2023-03-07 19:02:06 -0300

    update config env & add .env example

--- api/src/config/env.ts
+++ api/src/config/env.ts
@@ -1,6 +1,17 @@
-const dotenv = require("dotenv");
-dotenv.config();
+import dotenv from 'dotenv'
 
-const { PAYPAL_API_ClientID, PAYPAL_API_SECRET, PAYPAL_API_URL } = process.env;
+dotenv.config()
 
-module.exports = { PAYPAL_API_ClientID, PAYPAL_API_SECRET, PAYPAL_API_URL };
+export const config = {
+    PORT: process.env.PORT ?? 3000,
+    SECRET: process.env.SECRET,
+    NODE_ENV: process.env.NODE_ENV ?? 'development',
+    ORIGIN_CORS:
+        process.env.NODE_ENV === 'production' ? process.env.ORIGIN_CORS : '*',
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+    PAYPAL_API_CLIENTID: process.env.PAYPAL_API_CLIENTID,
+    PAYPAL_API_SECRET: process.env.PAYPAL_API_SECRET,
+    PAYPAL_API_URL: process.env.PAYPAL_API_URL,
+}
