303022e6573baf94c941dfb86c3cadced70f46f6
Luem <luchemma@gmail.com>
2023-03-08 15:42:52 -0300

    update commonjs to es6

--- api/src/controllers/paypal.ts
+++ api/src/controllers/paypal.ts
@@ -1,87 +1,82 @@
-const axios = require("axios");
+import axios from 'axios'
 // const paypal = require("../../../client/src/components/Paypal/PaymentAcepted")
-const {
-	PAYPAL_API_ClientID,
-	PAYPAL_API_SECRET,
-	PAYPAL_API_URL,
-} = require("../../config");
+import { config } from '../config/env'
 
-module.exports = {
-	create: async (req, res) => {
-		console.log("req.body", req.body);
-		try {
-			const order = {
-				intent: "CAPTURE",
-				purchase_units: req.body,
-				application_context: {
-					brand_name: "RGBTech",
-					locale: "en-US",
-					landing_page: "NO_PREFERENCE", //LOGIN
-					user_action: "PAY_NOW",
-					return_url: "http://localhost:3003/capture-order",
-					cancel_url: "http://localhost:3003/cancel-order",
-				},
-			};
+export async function create(req, res) {
+    console.log('req.body', req.body)
+    try {
+        const order = {
+            intent: 'CAPTURE',
+            purchase_units: req.body,
+            application_context: {
+                brand_name: 'RGBTech',
+                locale: 'en-US',
+                landing_page: 'NO_PREFERENCE', // LOGIN
+                user_action: 'PAY_NOW',
+                return_url: 'http://localhost:3003/capture-order',
+                cancel_url: 'http://localhost:3003/cancel-order',
+            },
+        }
 
-			const params = new URLSearchParams();
-			params.append("grant_type", "client_credentials");
+        const params = new URLSearchParams()
+        params.append('grant_type', 'client_credentials')
 
-			const {
-				data: { access_token },
-			} = await axios.post(
-				"https://api-m.sandbox.paypal.com/v1/oauth2/token",
-				params,
-				{
-					headers: {
-						"Content-Type": "application/x-www-form-urlencoded",
-					},
-					auth: {
-						username: PAYPAL_API_ClientID,
-						password: PAYPAL_API_SECRET,
-					},
-				}
-			);
+        const {
+            data: { access_token },
+        } = await axios.post(
+            'https://api-m.sandbox.paypal.com/v1/oauth2/token',
+            params,
+            {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                auth: {
+                    username: config.PAYPAL_API_CLIENTID,
+                    password: config.PAYPAL_API_SECRET,
+                },
+            }
+        )
 
-			const response = await axios.post(
-				`${PAYPAL_API_URL}/v2/checkout/orders`,
-				order,
-				{
-					headers: {
-						Authorization: `Bearer ${access_token}`,
-					},
-				}
-			);
+        const response = await axios.post(
+            `${config.PAYPAL_API_URL}/v2/checkout/orders`,
+            order,
+            {
+                headers: {
+                    Authorization: `Bearer ${access_token}`,
+                },
+            }
+        )
 
-			// console.log("response", response);
-			res.json(response.data.links[1].href);
-		} catch (error) {
-			console.log(error, "---e---");
-			res.send(error);
-		}
-	},
+        // console.log("response", response);
+        res.json(response.data.links[1].href)
+    } catch (error) {
+        console.log(error, '---e---')
+        res.send(error)
+    }
+}
 
-	capture: async (req, res) => {
-		try {
-			const { token, PayerID } = req.query;
+export async function capture(req, res) {
+    try {
+        const { token, PayerID } = req.query
 
-			const response = await axios.post(
-				`https://api-m.sandbox.paypal.com/v2/checkout/orders/${token}/capture`,
-				{},
-				{
-					auth: {
-						username: PAYPAL_API_ClientID,
-						password: PAYPAL_API_SECRET,
-					},
-				}
-			);
-			console.log("response.data", response.data);
-			res.redirect("http://localhost:5173/order-successfully");
-			// res.json(response.data)
-		} catch (error) {
-			console.log(error, "error");
-		}
-	},
-	cancel: (req, res) => {
-		res.redirect("http://localhost:5173/order-canceled");
-	},
-};
+        const response = await axios.post(
+            `https://api-m.sandbox.paypal.com/v2/checkout/orders/${token}/capture`,
+            {},
+            {
+                auth: {
+                    username: config.PAYPAL_API_CLIENTID,
+                    password: config.PAYPAL_API_SECRET,
+                },
+            }
+        )
+        console.log('response.data', response.data)
+        res.redirect('http://localhost:5173/order-successfully')
+        // res.json(response.data)
+    } catch (error) {
+        console.log(error, 'error')
+    }
+}
+
+export function cancel(req, res) {
+    res.redirect('http://localhost:5173/order-canceled')
+}
--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,39 +1,30 @@
-const { Router } = require("express");
-const productsRoute = require("./Product");
-const usersRoute = require("./User.js");
-const brandsRoute = require("./Brand.js");
-const tagsRoute = require("./Tag.js");
-const adminRoute = require("./Admin.js");
-const awardsRoute = require("./awards.js")
-const Updates = require("./Updates.js")
+import { Router } from 'express'
 
-const salesRoute = require("./Sales.js")
+import { create, capture, cancel } from '../controllers/paypal'
 
-const recoverPasswordRouter = require("./recoverPassword")
-const { cancel, create, capture } = require("../controllers/paypal");
-// const dogmiddleware = require('./middlewares/dogs.js')
-// const temperamentmiddleware = require('./middlewares/temperaments.js')
-// Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');
+import productsRoute from './product.route'
+import usersRoute from './user.route.js'
+import brandsRoute from './brand.route.js'
+import tagsRoute from './tag.route.js'
+import adminRoute from './admin.route.js'
+import awardsRoute from './awards.route.js'
+import Updates from './updates.route.js'
+import salesRoute from './sales.route.js'
+import recoverPasswordRouter from './recoverPassword.route'
 
-const router = Router();
-router.use("/products", productsRoute);
-router.use("/users", usersRoute);
-router.use("/brands", brandsRoute);
-router.use("/tags", tagsRoute);
-router.use("/admin", adminRoute);
-router.use("/awards", awardsRoute)
-router.use("/updates", Updates)
-router.use("/sales", salesRoute)
-router.use("/recoverPassword", recoverPasswordRouter )
-router.post("/create-order", create);
-router.get("/capture-order", capture);
-router.get("/cancel-order", cancel);
+const router = Router()
 
-// Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
+router.use('/products', productsRoute)
+router.use('/users', usersRoute)
+router.use('/brands', brandsRoute)
+router.use('/tags', tagsRoute)
+router.use('/admin', adminRoute)
+router.use('/awards', awardsRoute)
+router.use('/updates', Updates)
+router.use('/sales', salesRoute)
+router.use('/recoverPassword', recoverPasswordRouter)
+router.post('/create-order', create)
+router.get('/capture-order', capture)
+router.get('/cancel-order', cancel)
 
-// router.use('/dogs', dogmiddleware);
-// router.use('/temperaments', temperamentmiddleware);
-
-module.exports = router;
+export default router
