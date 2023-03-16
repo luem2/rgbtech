94e61091a139a4dfb3beb894bfcdbfd45ce168f5
Luem <luchemma@gmail.com>
2023-03-16 04:00:41 -0300

    update products seed

--- api/src/database/seeders/products/index.ts
+++ api/src/database/seeders/products/index.ts
@@ -1,37 +1,40 @@
-const { products } = require('./product.js')
-const { Product } = require('../db')
-const createAllProducts = () => {
-    products.map(async (product, index) => {
-        const {
-            name,
-            sales,
-            description,
-            price,
-            specifications,
-            img,
-            stock,
-            onDiscount,
-            discountPercentage,
-            freeShipping,
-            brand,
-            tag,
-        } = product
-        const newProduct = await Product.create({
-            name,
-            description,
-            price,
-            specifications,
-            img,
-            stock,
-            onDiscount,
-            discountPercentage,
-            freeShipping,
-            sales,
-        })
-        await newProduct.setBrand(brand)
-        await newProduct.addTags(tag)
-    })
-    console.log('Products added to db')
-}
+import { db } from '../../'
 
-module.exports = createAllProducts
+import products from './products.json'
+
+export async function createProducts(): Promise<void> {
+    try {
+        products.forEach(async (product) => {
+            const tags = product.tags.map((tag) => {
+                return {
+                    name: tag,
+                }
+            })
+
+            await db.product.create({
+                data: {
+                    name: product.name,
+                    price: product.price,
+                    description: product.description,
+                    specifications: product.specifications,
+                    picture: product.picture,
+                    stock: product.stock,
+                    onDiscount: product.onDiscount,
+                    discountPercentage: product.discountPercentage,
+                    freeShipping: product.freeShipping,
+                    brandId: product.brandId,
+                    tags: {
+                        connect: tags,
+                    },
+                },
+            })
+        })
+
+        console.info('☑️ Products successfully seeded in the database')
+    } catch (error) {
+        console.error(error)
+        process.exit(1)
+    } finally {
+        await db.$disconnect()
+    }
+}
