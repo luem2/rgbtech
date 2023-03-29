e39406a2f5fa612e6acbd51688d80ecf226f5cad
Luem <luchemma@gmail.com>
2023-03-29 19:36:55 -0300

    mini fixes (edit name file & delete old file

--- api/src/helpers/dto/user.schema.ts
+++ api/src/helpers/dto/user.schema.ts
@@ -1,6 +1,6 @@
 import { date, object, string } from 'yup'
 
-export default object({
+export const createUserSchema = object({
     body: object({
         firstName: string().required('Firstname is required'),
         lastName: string().required('Lastname is required'),
@@ -17,4 +17,14 @@ export default object({
         picture: string().url().optional(),
         birthDate: date().optional().nullable().min(new Date(1900, 0, 1)),
     }),
+}) as never
+
+export const editUserSchema = object({
+    firstName: string().required('Firstname is required'),
+    lastName: string().required('Lastname is required'),
+    email: string()
+        .email('Must be a valid email')
+        .required('Email is required'),
+    nacionality: string().required('Nacionality is required'),
+    birthDate: date().optional().nullable().min(new Date(1900, 0, 1)),
 }) as never
