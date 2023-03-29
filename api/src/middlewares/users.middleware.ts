9fd1b63ea63b60b5b1ca8a41ad6175b784e9f5a2
Luem <luchemma@gmail.com>
2023-03-29 19:35:40 -0300

    add validations avatar, oldPassword & email

--- api/src/middlewares/users.middleware.ts
+++ api/src/middlewares/users.middleware.ts
@@ -1,29 +1,87 @@
 import type { User } from '@prisma/client'
 import type { NextFunction, Request, Response } from 'express'
+import type multer from 'multer'
+
+import { compare } from 'bcrypt'
 
 import { db } from '../database'
 
-// import jwt from 'jsonwebtoken'
-// import nodemailer from 'nodemailer'
-// import bcrypt from 'bcrypt'
-
-// import { htmlMail, htmlMailSuccessfulPayment } from '../Utils/EmailTemplate.js'
-
 class UsersMiddlewares {
-    async checkUserProfile(
+    async checkUserEmailUpdateProfile(
         req: Request,
         res: Response,
         next: NextFunction
     ): Promise<void> {
-        // const user = (await db.user.findUnique({
-        //     where: {
-        //         id: req.userId,
-        //     },
-        // })) as User
+        const user = await db.user.findUnique({
+            where: {
+                id: req.userId,
+            },
+            select: {
+                email: true,
+            },
+        })
 
-        // const pepe = req.body.user ? '' : 'a'
+        if ((user as User).email === req.body.email) {
+            next()
+        }
+
+        const otherUser = await db.user.findUnique({
+            where: {
+                email: req.body.email,
+            },
+        })
+
+        if (otherUser) {
+            res.status(401).send({
+                status: 'Error',
+                msg: 'A user has already registered with the email address entered',
+            })
+        } else {
+            next()
+        }
+    }
+
+    async checkUserOldPassword(
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ): Promise<Response<unknown, Record<string, unknown>> | undefined> {
+        const user = (await db.user.findUnique({
+            where: {
+                id: req.userId,
+            },
+            select: {
+                password: true,
+            },
+        })) as User
+
+        const passwordMatches = await compare(
+            req.body.oldPassword,
+            user?.password
+        )
+
+        if (!passwordMatches) {
+            return res.status(401).send({
+                status: 'Error',
+                msg: 'The sent password does not match the current password',
+            })
+        }
 
         next()
+    }
+
+    filePhotoProfileFilter(
+        _req: Request,
+        file: Express.Multer.File,
+        cb: multer.FileFilterCallback
+    ): void {
+        const mimeTypesAccepted = ['image/png', 'image/jpeg', 'image/jpg']
+
+        if (mimeTypesAccepted.includes(file.mimetype)) {
+            cb(null, true)
+        } else {
+            cb(new Error('Only PNG, JPEG and JPG files are allowed'))
+        }
     }
 }
 
