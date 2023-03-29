a09b6423534689f84f073c975f98a532e403cc81
Luem <luchemma@gmail.com>
2023-03-29 19:36:08 -0300

    add users controllers (password & avatar)

--- api/src/controllers/users.controller.ts
+++ api/src/controllers/users.controller.ts
@@ -1,5 +1,6 @@
 import type { Request, Response } from 'express'
 
+import authServices from '../services/auth.service'
 import usersServices from '../services/users.service'
 
 class UsersController {
@@ -13,15 +14,33 @@ class UsersController {
         })
     }
 
-    async changeProfile(req: Request, res: Response): Promise<void> {
-        const profileEdited = await usersServices.changeProfile(req)
+    async updateProfile(req: Request, res: Response): Promise<void> {
+        const profileEdited = await usersServices.updateProfile(req)
 
         res.status(200).send({
             status: 'Success',
-            msg: 'All users were sent correctly',
+            msg: 'The profile was successfully updated',
             body: profileEdited,
         })
     }
-}
 
+    async updatePassword(req: Request, res: Response): Promise<void> {
+        const passwordUpdated = await authServices.passwordUpdate(req)
+
+        res.status(200).send({
+            status: 'Success',
+            msg: 'The password was successfully updated',
+            body: passwordUpdated,
+        })
+    }
+
+    async changeProfilePhoto(req: Request, res: Response): Promise<void> {
+        await usersServices.changeProfilePhoto(req)
+
+        res.status(200).send({
+            status: 'Success',
+            msg: 'The profile photo was successfully updated',
+        })
+    }
+}
 export default new UsersController()
