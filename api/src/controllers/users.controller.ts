3c17b0f2ebfd123a4e1dd18c1d2b8b53f5a6a18d
Luem <luchemma@gmail.com>
2023-03-30 23:16:10 -0300

    add shopping cart controllers

--- api/src/controllers/users.controller.ts
+++ api/src/controllers/users.controller.ts
@@ -17,7 +17,7 @@ class UsersController {
     async updateProfile(req: Request, res: Response): Promise<void> {
         const profileEdited = await usersServices.updateProfile(req)
 
-        res.status(200).send({
+        res.status(201).send({
             status: 'Success',
             msg: 'The profile was successfully updated',
             body: profileEdited,
@@ -25,21 +25,72 @@ class UsersController {
     }
 
     async updatePassword(req: Request, res: Response): Promise<void> {
-        const passwordUpdated = await authServices.passwordUpdate(req)
+        const userWithPasswordUpdated = await authServices.passwordUpdate(req)
 
-        res.status(200).send({
+        res.status(201).send({
             status: 'Success',
             msg: 'The password was successfully updated',
-            body: passwordUpdated,
+            body: userWithPasswordUpdated,
         })
     }
 
     async changeProfilePhoto(req: Request, res: Response): Promise<void> {
         await usersServices.changeProfilePhoto(req)
 
-        res.status(200).send({
+        res.status(201).send({
             status: 'Success',
             msg: 'The profile photo was successfully updated',
+        })
+    }
+
+    async getShoppingCart(req: Request, res: Response): Promise<void> {
+        const user = await usersServices.getShoppingCart(req)
+
+        res.status(200).send({
+            status: 'Success',
+            msg: `The user's shopping cart was successfully submitted`,
+            body: user?.shoppingCart,
+        })
+    }
+
+    async addItemToCart(req: Request, res: Response): Promise<void> {
+        await usersServices.addItemToCart(req)
+
+        const product = req.body.productId as string
+
+        res.status(201).send({
+            status: 'Success',
+            msg: `The item ${product} has been added to cart`,
+            body: req.body.productId,
+        })
+    }
+
+    async modifyItemQuantity(req: Request, res: Response): Promise<void> {
+        await usersServices.modifyItemQuantity(req)
+
+        res.status(201).send({
+            status: 'Success',
+            msg: 'The item quantity has been modified',
+        })
+    }
+
+    async deleteItemFromCart(req: Request, res: Response): Promise<void> {
+        const product = req.params.productId
+
+        await usersServices.deleteItemFromCart(req)
+
+        res.status(201).send({
+            status: 'Success',
+            msg: `The item ${product} has been deleted from cart`,
+        })
+    }
+
+    async cleanShoppingCart(req: Request, res: Response): Promise<void> {
+        await usersServices.cleanShoppingCart(req)
+
+        res.status(201).send({
+            status: 'Success',
+            msg: 'The shopping cart was cleaned',
         })
     }
 }
