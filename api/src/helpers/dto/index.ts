1d4ce9ae86e1b5e18a84de077c1f29f33e41083b
Luem <luchemma@gmail.com>
2023-04-04 23:30:41 -0300

    add productSchema

--- api/src/helpers/dto/index.ts
+++ api/src/helpers/dto/index.ts
@@ -5,3 +5,4 @@ export {
 } from './user.schema'
 export { default as newPasswordSchema } from './newPassword.schema'
 export { default as emailSchema } from './email.schema'
+export { productSchema } from './product.schema'
--- api/src/helpers/dto/product.schema.ts
+++ api/src/helpers/dto/product.schema.ts
@@ -1,34 +1,29 @@
-import { date, object, string } from 'yup'
+import { array, boolean, number, object, string } from 'yup'
 
-// TODO: CREATE PRODUCT SCHEMA
-
-export const createProductSchema = object({
+export const productSchema = object({
     body: object({
-        firstName: string().required('Firstname is required'),
-        lastName: string().required('Lastname is required'),
-        email: string()
-            .email('Must be a valid email')
-            .required('Email is required'),
-        password: string()
-            .matches(
-                /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).*$/,
-                'The password must contain at least one uppercase letter, one lowercase letter, one number and one special character.'
-            )
-            .required('Password is required'),
-        nacionality: string().optional(),
-        picture: string().url().optional(),
-        birthDate: date().optional().nullable().min(new Date(1900, 0, 1)),
+        name: string().required('Name is required'),
+        price: number().required('Price is required'),
+        description: string().required('Description is required'),
+        specifications: object().required('Specifications must be an object'),
+        picture: string().required('Picture is required'),
+        stock: number().required('Stock is required'),
+        onDiscount: boolean().required('OnDiscount is required'),
+        discountPercentage: number().required('DiscountPercentage is required'),
+        freeShipping: boolean().required('FreeShipping is required'),
+        rating: number()
+            .nullable()
+            .required('Rating is required (can be null)'),
+        tags: array().of(
+            object({
+                name: string().required(`Name is required`),
+                disabled: boolean().required('Disabled boolean is required'),
+            })
+        ),
+        brand: object({
+            name: string().required('Name is required'),
+            logo: string().required('Logo is required'),
+            disabled: boolean().required('Disabled boolean is required'),
+        }),
     }),
-}) as never
-
-// TODO: EDIT PRODUCT SCHEMA
-
-export const editProductSchema = object({
-    firstName: string().required('Firstname is required'),
-    lastName: string().required('Lastname is required'),
-    email: string()
-        .email('Must be a valid email')
-        .required('Email is required'),
-    nacionality: string().required('Nacionality is required'),
-    birthDate: date().optional().nullable().min(new Date(1900, 0, 1)),
 }) as never
