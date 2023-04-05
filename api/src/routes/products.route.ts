a619557d9a15224f37e58fc4268db99e27baaac1
Luem <luchemma@gmail.com>
2023-04-05 15:47:30 -0300

    feat: :zap: adding middlewares on routes, and new routes.

--- api/src/routes/products.route.ts
+++ api/src/routes/products.route.ts
@@ -2,20 +2,56 @@ import { Router } from 'express'
 
 import productsController from '../controllers/products.controller'
 import authMiddlewares from '../middlewares/auth.middleware'
+import productsMiddlewares from '../middlewares/products.middleware'
+import { productSchema } from '../helpers/dto/product.schema'
+import { validateSchema } from '../helpers/validateRequest'
 
 const router = Router()
 
 router
-    .get('/', productsController.getAllProducts)
+    // TODO: Agregar Multiples filtros con QUERY (Fijarse que filtros hay en rgbtech.vercel.app y agregar nuevos)
+    // [ ] freeShipping
+    // [ ] onDiscount
+    // [ ] rating
+    .get(
+        '/',
+        productsMiddlewares.getProductsAuthMiddleware,
+        productsController.getAllProducts
+    )
 
-    .get('/:productId', productsController.getProduct)
+    .get(
+        '/:productId',
+        productsMiddlewares.getProductsAuthMiddleware,
+        productsController.getProduct
+    )
 
     .put(
-        '/:productId',
-        authMiddlewares.checkAdminAuth,
+        '/',
+        [
+            authMiddlewares.checkAdminAuth,
+            validateSchema(productSchema),
+            productsMiddlewares.checkBodyEditProduct,
+        ],
         productsController.productUpdate
     )
 
-    .patch('/:productId', authMiddlewares.checkAdminAuth)
+    .post(
+        '/',
+        [
+            authMiddlewares.checkAdminAuth,
+            validateSchema(productSchema),
+            productsMiddlewares.checkBodyAddProduct,
+        ],
+        productsController.addProduct
+    )
+
+    .patch(
+        '/:productId',
+        [
+            authMiddlewares.checkAdminAuth,
+            productsMiddlewares.checkUpdateProductAvailability,
+        ],
+        productsController.changeProductAvailability
+    )
 
 export default router
