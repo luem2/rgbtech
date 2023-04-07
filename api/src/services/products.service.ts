14d6ecd5033d506b6742d6f2969650c4d9bfa41d
Luem <luchemma@gmail.com>
2023-04-07 19:11:02 -0300

    query products endpoint advance 1

--- api/src/services/products.service.ts
+++ api/src/services/products.service.ts
@@ -1,56 +1,142 @@
+import type { Prisma } from '@prisma/client'
 import type { Request } from 'express'
 
 import { db } from '../database'
 
 class ProductsServices {
-    async getAllProducts(req: Request) {
-        console.log('req.query', req.query)
+    readonly userInfoSelectedToSubmit: Prisma.ProductSelect = {
+        _count: {
+            select: {
+                usersFavorite: true,
+                reviews: true,
+                tags: true,
+                transactions: true,
+            },
+        },
+        id: true,
+        name: true,
+        description: true,
+        price: true,
+        specifications: true,
+        picture: true,
+        stock: true,
+        onDiscount: true,
+        discountPercentage: true,
+        freeShipping: true,
+        rating: true,
 
-        if (req.userRole !== 'ADMIN') {
+        tags: true,
+        brand: true,
+        reviews: true,
+    }
+
+    readonly adminInfoIncludedToSubmit: Prisma.ProductInclude = {
+        _count: true,
+        brand: true,
+        reviews: true,
+        shoppingCarts: true,
+        tags: true,
+        transactions: true,
+        usersFavorite: true,
+        usersHistory: true,
+    }
+
+    async getAllProducts({ userRole }: Request) {
+        if (userRole !== 'ADMIN') {
             return await db.product.findMany({
                 where: {
                     disabled: {
                         not: true,
                     },
                 },
-                select: {
-                    _count: {
-                        select: {
-                            usersFavorite: true,
-                            reviews: true,
-                            tags: true,
-                        },
-                    },
-                    id: true,
-                    name: true,
-                    description: true,
-                    price: true,
-                    specifications: true,
-                    picture: true,
-                    stock: true,
-                    onDiscount: true,
-                    discountPercentage: true,
-                    freeShipping: true,
-                    rating: true,
-
-                    tags: true,
-                    brand: true,
-                    reviews: true,
-                },
+                select: this.userInfoSelectedToSubmit,
             })
         } else
             return await db.product.findMany({
-                include: {
-                    _count: true,
-                    brand: true,
-                    reviews: true,
-                    shoppingCarts: true,
-                    tags: true,
-                    transactions: true,
-                    usersFavorite: true,
-                    usersHistory: true,
-                },
+                include: this.adminInfoIncludedToSubmit,
             })
+    }
+
+    async getQueryProducts({ query, userRole }: Request) {
+        const queryObject = query as Record<string, string>
+        const queryParams = new URLSearchParams(queryObject)
+
+        const queryProductsFilter: Prisma.ProductWhereInput = {
+            brand: {
+                name: queryObject.brand
+                    ? {
+                          equals: queryParams.get('brand') as string,
+                          mode: 'insensitive',
+                      }
+                    : undefined,
+            },
+
+            name: queryObject.name
+                ? {
+                      contains: queryParams.get('name') as string,
+                      mode: 'insensitive',
+                  }
+                : undefined,
+
+            price: queryObject.price
+                ? {
+                      gte: Number(queryParams.get('greaterThan')),
+                      lte: Number(queryParams),
+                  }
+                : undefined,
+
+            rating: queryObject.rating
+                ? {
+                      //   lte: 5,
+                      //   gte: 2,
+                      //   equals: 5,
+                  }
+                : undefined,
+
+            tags: {
+                every: {
+                    name: queryObject.tags
+                        ? {
+                              in: queryParams.getAll('tags'),
+                              mode: 'insensitive',
+                          }
+                        : undefined,
+                },
+            },
+
+            stock: queryObject.stock
+                ? {
+                      // gte: 2,
+                      // lte: 2,
+                  }
+                : undefined,
+
+            onDiscount: {
+                equals: queryObject.onDiscount
+                    ? JSON.parse(queryParams.get('onDiscount') as string)
+                    : undefined,
+            },
+
+            freeShipping: {
+                equals: queryObject.freeShipping
+                    ? JSON.parse(queryObject.freeShipping)
+                    : undefined,
+            },
+
+            disabled:
+                userRole !== 'ADMIN'
+                    ? {
+                          equals: false,
+                      }
+                    : undefined,
+        }
+
+        return await db.product.findMany({
+            where: queryProductsFilter,
+            // orderBy: {},
+            // select: userRole !== 'ADMIN' ? this.userInfoSelectedToSubmit : undefined,
+            // include: userRole === 'ADMIN' ? this.adminInfoIncludedToSubmit : undefined,
+        })
     }
 
     async getProduct(req: Request) {
