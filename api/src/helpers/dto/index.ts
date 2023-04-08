6451f91bc6eccbeafb3796c778b1502f0a9f4a51
Luem <luchemma@gmail.com>
2023-04-08 19:09:51 -0300

    edit barrel

--- api/src/helpers/dto/index.ts
+++ api/src/helpers/dto/index.ts
@@ -1,8 +1,5 @@
-export {
-    createUserSchema,
-    editUserSchema,
-    createUserSchemaWithGoogle,
-} from './user.schema'
-export { default as newPasswordSchema } from './newPassword.schema'
 export { default as emailSchema } from './email.schema'
-export { productSchema } from './product.schema'
+export { default as newPasswordSchema } from './newPassword.schema'
+export * from './product.schema'
+export * from './query.schema'
+export * from './user.schema'
