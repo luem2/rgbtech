4a750ff1226aad8b65e2598b89e27e814e8d6aca
Luem <luchemma@gmail.com>
2023-04-13 18:53:50 -0300

    edit product schema

--- api/src/helpers/dto/product.schema.ts
+++ api/src/helpers/dto/product.schema.ts
@@ -11,19 +11,7 @@ export const productSchema = object({
         onDiscount: boolean().required('OnDiscount is required'),
         discountPercentage: number().required('DiscountPercentage is required'),
         freeShipping: boolean().required('FreeShipping is required'),
-        rating: number()
-            .nullable()
-            .required('Rating is required (can be null)'),
-        tags: array().of(
-            object({
-                name: string().required(`Name is required`),
-                disabled: boolean().required('Disabled boolean is required'),
-            })
-        ),
-        brand: object({
-            name: string().required('Name is required'),
-            logo: string().required('Logo is required'),
-            disabled: boolean().required('Disabled boolean is required'),
-        }),
+        tags: array().of(string()),
+        brand: string(),
     }),
 }) as never
