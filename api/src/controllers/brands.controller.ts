1fab3e623cdecdeff3afa5bbe941bda0905b01c9
Luem <luchemma@gmail.com>
2023-04-13 23:41:38 -0300

    add all brand controllers

---
+++ api/src/controllers/brands.controller.ts
@@ -0,0 +1,70 @@
+import type { Request, Response } from 'express'
+
+import brandsServices from '../services/brands.service'
+
+class BrandsControllers {
+    async getAllBrands(_req: Request, res: Response) {
+        const brands = await brandsServices.getAllBrands()
+
+        res.status(200).send({
+            status: 'Success',
+            msg: 'All brands have been successfully sent',
+            body: {
+                brands,
+                count: brands.length,
+            },
+        })
+    }
+
+    async getBrand(req: Request, res: Response) {
+        const brand = await brandsServices.getBrand(req.params)
+
+        if (brand) {
+            res.status(200).send({
+                status: 'Success',
+                msg: 'The brand has been successfully sent',
+                body: brand,
+            })
+        } else {
+            res.status(404).send({
+                status: 'Error',
+                msg: 'The brand has not been found',
+                body: brand,
+            })
+        }
+    }
+
+    async brandUpdate(req: Request, res: Response) {
+        const updatedBrand = await brandsServices.brandUpdate(req)
+
+        res.status(200).send({
+            status: 'Success',
+            msg: 'Brand have been successfully updated',
+            body: updatedBrand,
+        })
+    }
+
+    async addBrand(req: Request, res: Response) {
+        const newProduct = await brandsServices.addBrand(req.body)
+
+        res.status(201).send({
+            status: 'Success',
+            msg: 'Brand have been successfully created',
+            body: newProduct,
+        })
+    }
+
+    async changeBrandAvailability(req: Request, res: Response) {
+        const productDisabled = await brandsServices.changeBrandAvailability(
+            req
+        )
+
+        res.status(200).send({
+            status: 'Success',
+            msg: 'Brand have been successfully updated',
+            body: productDisabled,
+        })
+    }
+}
+
+export default new BrandsControllers()
