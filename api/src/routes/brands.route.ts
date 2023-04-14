c00750893b4bde7c53c8a8d4863d0e541df828d1
Luem <luchemma@gmail.com>
2023-04-13 23:42:22 -0300

    add brand routes

--- api/src/routes/brands.route.ts
+++ api/src/routes/brands.route.ts
@@ -1,35 +1,46 @@
 import { Router } from 'express'
-import { Brand } from '../db.js'
+
+import brandsControllers from '../controllers/brands.controller'
+import authMiddlewares from '../middlewares/auth.middleware'
+import brandsMiddlewares from '../middlewares/brands.middleware'
+import { validateSchema } from '../helpers/validateRequest'
+import { brandSchema } from '../helpers/dto'
 
 const router = Router()
 
-router.get('/', async (req, res) => {
-    try {
-        const brands = await Brand.findAll({
-            attributes: ['id', 'name'],
-        })
-        res.status(200).send(brands)
-    } catch (error) {
-        res.status(500).send('Internal error server')
-    }
-})
+router
 
-router.post('/', async (req, res) => {
-    let { name, id, logo } = req.body
-    if (!name || !id)
-        return res.status(404).send('Falta enviar datos obligatorios')
-    try {
-        console.log('entre al try')
-        await Brand.create({
-            name,
-            id,
-            logo,
-        })
-        return res.send('Brand created successfully')
-    } catch (error) {
-        console.log('entre al catch')
-        return res.status(404).send('Error en alguno de los datos provistos')
-    }
-})
+    .get('/', authMiddlewares.checkAdminAuth, brandsControllers.getAllBrands)
+
+    .get('/:name', authMiddlewares.checkAdminAuth, brandsControllers.getBrand)
+
+    .put(
+        '/:name',
+        [
+            authMiddlewares.checkAdminAuth,
+            validateSchema(brandSchema),
+            brandsMiddlewares.checkBodyEditBrand,
+        ],
+        brandsControllers.brandUpdate
+    )
+
+    .post(
+        '/',
+        [
+            authMiddlewares.checkAdminAuth,
+            validateSchema(brandSchema),
+            brandsMiddlewares.checkBodyAddBrand,
+        ],
+        brandsControllers.addBrand
+    )
+
+    .patch(
+        '/:name',
+        [
+            authMiddlewares.checkAdminAuth,
+            brandsMiddlewares.checkUpdateBrandAvailability,
+        ],
+        brandsControllers.changeBrandAvailability
+    )
 
 export default router
