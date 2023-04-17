ea8e85e844850ce3bbf1dcca668438247c36f14a
Luem <luchemma@gmail.com>
2023-04-17 00:01:40 -0300

    delete useless property in Express namespace

--- api/src/types/index.d.ts
+++ api/src/types/index.d.ts
@@ -1,4 +1,4 @@
-import type { Prisma, Product } from '@prisma/client'
+import type { Prisma } from '@prisma/client'
 
 export {}
 declare global {
@@ -7,7 +7,6 @@ declare global {
             userId: string
             userRole: string
             parsedQuery: Record<string, unknown>
-            product: Product
         }
     }
 }
