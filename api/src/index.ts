bbf7889473ca5111dc793983170a2749b9f43210
Luem <luchemma@gmail.com>
2023-04-20 16:27:40 -0300

    move server in to index

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,40 +1,78 @@
-/* 
-    ███████████     █████████  ███████████  ███████████ ██████████   █████████  █████   █████
-    ░███    ░███  ███     ░░░  ░███    ░███░   ░███  ░  ░███  █ ░  ███     ░░░  ░███    ░███
-    ░██████████  ░███          ░██████████     ░███     ░██████   ░███          ░███████████
-    ░███░░░░░███ ░███    █████ ░███░░░░░███    ░███     ░███░░█   ░███          ░███░░░░░███
-    ░███    ░███ ░░███  ░░███  ░███    ░███    ░███     ░███ ░   █░░███     ███ ░███    ░███
-    █████   █████ ░░█████████  ███████████     █████    ██████████ ░░█████████  █████   █████
-    ░░░░░   ░░░░░   ░░░░░░░░░  ░░░░░░░░░░░     ░░░░░    ░░░░░░░░░░   ░░░░░░░░░  ░░░░░   ░░░░░
+import type { Application } from 'express'
 
-                                            _..._
-                                          .'     '.    ___
-                                       .-|   /:.   |  |   |
-                                       |  \  |:.   /.-'-./
-                                       | .-'-;:__.'    =/
-                                       .'=  *=|      _.='
-                                      /   _.  |    ;
-                                     ;-.-'|    \   |
-                                    /   | \    _\  _\
-                                    \__/'._;.  ==' ==\
-                                             \    \   |
-                                             /    /   /
-                                             /-._/-._/
-                                            \   `\  \
-                                              `-._/._/
+import path from 'path'
+import { readFileSync } from 'fs'
 
-*/
+import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
+import cookieParser from 'cookie-parser'
 
-import Server from './config/server'
+import routes from './routes'
+import { errorHandler } from './middlewares/errorHandler'
+import { ConfigServer } from './config'
 
-async function bootstrap() {
-    try {
-        const server = new Server()
+class ServerBootstrap extends ConfigServer {
+    readonly app: Application
+    readonly port: number
 
-        server.listen()
-    } catch (error) {
-        console.error(error)
+    constructor() {
+        super()
+
+        this.app = express()
+        this.port = this.getNumberEnv('PORT') ?? 3000
+
+        this.middlewares()
+        this.routes()
+        this.listen()
+    }
+
+    middlewares() {
+        this.app.use(
+            cors({
+                origin: this.getEnvironment('ORIGIN_CORS'),
+                // origin: config.ORIGIN_CORS,
+                credentials: true,
+                methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE'],
+                allowedHeaders: [
+                    'Origin',
+                    'X-Requested-With',
+                    'Content-Type',
+                    'Accept',
+                ],
+            })
+        )
+
+        this.app.use(morgan('dev'))
+        this.app.use(cookieParser())
+        this.app.use(
+            express.json({
+                limit: '50mb',
+            })
+        )
+
+        this.app.use(
+            express.urlencoded({
+                extended: true,
+                limit: '50mb',
+            })
+        )
+        this.app.use(express.static('public'))
+        this.app.use('/uploads', express.static('uploads'))
+    }
+
+    routes() {
+        this.app.use('/api', routes)
+        this.app.use(errorHandler)
+    }
+
+    listen() {
+        console.info(readFileSync(path.resolve('./src/banner.md'), 'utf-8'))
+
+        this.app.listen(this.port, () => {
+            console.info(`🟢 Server listening on port ${this.port}`)
+        })
     }
 }
 
-bootstrap()
+new ServerBootstrap()
