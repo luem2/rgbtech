85c7f5cc7778fac74464f2ed135272581e1572df
Luem <luchemma@gmail.com>
2023-04-25 18:34:45 -0300

    add checkCreateOrder middleware

--- api/src/middlewares/transactions.middlewares.ts
+++ api/src/middlewares/transactions.middlewares.ts
@@ -1,7 +1,37 @@
+import type { NextFunction, Request, Response } from 'express'
+import type { CartItem } from '@prisma/client'
+
 import { BaseMiddlewares } from '../config/bases'
+import { db } from '../database'
 
 export class TransactionMiddlewares extends BaseMiddlewares {
-    checkAddTransaction = async () => {
-        return 'Checking the transaction'
+    checkCreateOrder = async (
+        req: Request,
+        _res: Response,
+        next: NextFunction
+    ) => {
+        const user = (await db.user.findUnique({
+            where: {
+                id: req.userId,
+            },
+            select: {
+                shoppingCart: true,
+            },
+        })) as { shoppingCart: CartItem[] }
+
+        if (!user.shoppingCart.length) {
+            next(new this.HttpError(400, 'Cart is empty'))
+
+            return
+        }
+
+        const newOrder = user.shoppingCart.map((p) => ({
+            productId: p.productId,
+            amount: p.quantity,
+        }))
+
+        req.body = newOrder
+
+        next()
     }
 }
