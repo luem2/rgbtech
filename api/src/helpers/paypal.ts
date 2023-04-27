22649aadf7f963a49d6b3646da49ea4afe1f6661
Luem <luchemma@gmail.com>
2023-04-27 20:26:18 -0300

    init paypalAPI

---
+++ api/src/helpers/paypal.ts
@@ -0,0 +1,85 @@
+import type { AxiosResponse } from 'axios'
+
+import axios from 'axios'
+
+import { config } from '../config'
+
+export class PaypalApi {
+    declare CLIENTID
+    declare SECRET
+
+    constructor() {
+        this.CLIENTID = config.PAYPAL_API_CLIENTID
+        this.SECRET = config.PAYPAL_API_SECRET
+
+        axios.defaults.baseURL = config.PAYPAL_API_URL
+        axios.defaults.method = 'post'
+    }
+
+    async createOrder() {
+        const accessToken = await this.generateAccessToken()
+
+        const response = await axios({
+            url: '/v2/checkout/orders',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${accessToken}`,
+            },
+            data: JSON.stringify({
+                intent: 'CAPTURE',
+                purchase_units: [
+                    {
+                        amount: {
+                            currency_code: 'USD',
+                            value: '100.00',
+                        },
+                    },
+                ],
+            }),
+        })
+
+        return await this.handleResponse(response)
+    }
+
+    async capturePayment(orderId: string) {
+        const accessToken = await this.generateAccessToken()
+
+        const response = await axios({
+            url: `/v2/checkout/orders/${orderId}/capture`,
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${accessToken}`,
+            },
+        })
+
+        return await this.handleResponse(response)
+    }
+
+    async generateAccessToken() {
+        const auth = Buffer.from(this.CLIENTID + ':' + this.SECRET).toString(
+            'base64'
+        )
+
+        const response = await axios({
+            url: '/v1/oauth2/token',
+            headers: {
+                Authorization: `Basic ${auth}`,
+            },
+            data: {
+                grant_type: 'client_credentials',
+            },
+        })
+
+        const jsonData = await this.handleResponse(response)
+
+        return jsonData.access_token as string
+    }
+
+    async handleResponse(response: AxiosResponse) {
+        if (response.status === 200 || response.status === 201) {
+            return response.data
+        }
+
+        throw new Error(response.statusText)
+    }
+}
