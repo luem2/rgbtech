be0d9157acde7fdeeeca157902135f33d855d797
Luem <luchemma@gmail.com>
2023-05-02 17:51:17 -0300

    update query validations middleware & others

--- api/src/middlewares/products.middlewares.ts
+++ api/src/middlewares/products.middlewares.ts
@@ -5,7 +5,7 @@ import type { ProductSchema } from '../types'
 import { db } from '../database'
 import { verifyToken } from '../helpers/generateToken'
 import { generateFileName } from '../helpers/filename'
-import { writeNewFile } from '../helpers/fsFunctions'
+import { deleteFile, writeNewFile } from '../helpers/fsFunctions'
 import { CORE } from '../helpers/constants'
 import { BaseMiddlewares } from '../config/bases'
 
@@ -180,18 +180,6 @@ export class ProductMiddlewares extends BaseMiddlewares {
         _res: Response,
         next: NextFunction
     ) => {
-        const productFinded = await db.product.findUnique({
-            where: {
-                id: req.params.productId,
-            },
-        })
-
-        if (!productFinded) {
-            next(new this.HttpError(404, 'Product not found'))
-
-            return
-        }
-
         if (typeof req.body.disabled !== 'boolean') {
             next(
                 new this.HttpError(
@@ -206,30 +194,42 @@ export class ProductMiddlewares extends BaseMiddlewares {
         next()
     }
 
-    checkQueryObjectFilters = async (
+    checkQuerySchema = async (
         req: Request,
         _res: Response,
         next: NextFunction
     ) => {
         if (Object.keys(req.query).length) {
-            // const queryValidationSchema = await validateSchemaInsideMiddleware(
-            //     querySchema,
-            //     req
-            // )
-            // if (!queryValidationSchema.valid) {
-            //     next(
-            //         new ValidationError(
-            //             queryValidationSchema.err as ValidationError
-            //         )
-            //     )
-            //     return
-            // }
+            if (req.query.sortBy && !req.query.sortOrder) {
+                next(
+                    new this.HttpError(
+                        401,
+                        'The sortOrder property is needed to perform the operation'
+                    )
+                )
+
+                return
+            }
+
+            if (
+                req.query.rating &&
+                (req.query.rating_gte ?? req.query.rating_lte)
+            ) {
+                next(
+                    new this.HttpError(
+                        401,
+                        'The rating property can not have gte or lte properties'
+                    )
+                )
+
+                return
+            }
         }
 
         next()
     }
 
-    checkUpdatePictureProduct = (
+    checkUpdatePictureProduct = async (
         req: Request,
         _res: Response,
         next: NextFunction
@@ -241,6 +241,52 @@ export class ProductMiddlewares extends BaseMiddlewares {
                     'Product id is needed to perform the operation'
                 )
             )
+
+            return
+        }
+
+        const product = await db.product.findUnique({
+            where: {
+                id: req.query.id as string,
+            },
+            select: {
+                picture: true,
+            },
+        })
+
+        if (!product) {
+            next(new this.HttpError(404, 'Product not found'))
+
+            return
+        }
+
+        if (!req.file) {
+            next(new this.HttpError(401, 'The new product image is needed'))
+
+            return
+        }
+
+        deleteFile({
+            nameFolder: CORE,
+            fileName: product.picture.split('/').pop() as string,
+        })
+
+        next()
+    }
+
+    checkIfProductExists = async (
+        req: Request,
+        _res: Response,
+        next: NextFunction
+    ) => {
+        const product = await db.product.findUnique({
+            where: {
+                id: req.params.id,
+            },
+        })
+
+        if (!product) {
+            next(new this.HttpError(404, 'Product not found'))
 
             return
         }
--- api/src/routers/products.routes.ts
+++ api/src/routers/products.routes.ts
@@ -1,6 +1,6 @@
 import { ProductControllers } from '../controllers/products.controllers'
 import { ProductMiddlewares } from '../middlewares/products.middlewares'
-import { productSchema } from '../schemas/product.schemas'
+import { querySchema, productSchema } from '../schemas'
 import { validateSchema, parseRequest } from '../middlewares'
 import { BaseRouter } from '../config/bases'
 import multer from '../config/multer'
@@ -20,21 +20,25 @@ export class ProductRouter extends BaseRouter<
             [
                 this.middlewares.getProductsAuthMiddleware,
                 parseRequest('query'),
-                this.middlewares.checkQueryObjectFilters,
+                validateSchema(querySchema),
+                this.middlewares.checkQuerySchema,
             ],
             this.controllers.getAllProducts
         )
 
         this.router.get(
             '/:id',
-            this.middlewares.getProductsAuthMiddleware,
+            [
+                this.middlewares.getProductsAuthMiddleware,
+                this.middlewares.checkIfProductExists,
+            ],
             this.controllers.getProduct
         )
 
         this.router.put(
             '/',
+            this.auth.checkAdminAuth,
             [
-                this.auth.checkAdminAuth,
                 validateSchema(productSchema),
                 this.middlewares.checkBrandAndTags,
                 this.middlewares.checkBodyEditProduct,
@@ -44,9 +48,9 @@ export class ProductRouter extends BaseRouter<
 
         this.router.put(
             '/picture',
+            this.auth.checkAdminAuth,
             [
                 multer.single('product'),
-                this.auth.checkAdminAuth,
                 this.middlewares.checkUpdatePictureProduct,
             ],
             this.controllers.productPictureUpdate
@@ -54,9 +58,9 @@ export class ProductRouter extends BaseRouter<
 
         this.router.post(
             '/',
+            this.auth.checkAdminAuth,
             [
                 multer.single('product'),
-                this.auth.checkAdminAuth,
                 parseRequest('body'),
                 validateSchema(productSchema),
                 this.middlewares.checkBrandAndTags,
@@ -68,13 +72,15 @@ export class ProductRouter extends BaseRouter<
         this.router.delete(
             '/:id',
             this.auth.checkAdminAuth,
+            this.middlewares.checkIfProductExists,
             this.controllers.deleteProduct
         )
 
         this.router.patch(
             '/:id',
+            this.auth.checkAdminAuth,
             [
-                this.auth.checkAdminAuth,
+                this.middlewares.checkIfProductExists,
                 this.middlewares.checkUpdateProductAvailability,
             ],
             this.controllers.changeProductAvailability
