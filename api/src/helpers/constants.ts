839a9ba3b727d9fb787b36ce88c16fb8d222b769
Luem <luchemma@gmail.com>
2023-05-06 22:08:17 -0300

    adding constants and editing existing ones.

--- api/src/helpers/constants.ts
+++ api/src/helpers/constants.ts
@@ -1,3 +1,9 @@
-export const DEFAULT_AVATAR = 'avatar-default_image.png'
-export const CORE = 'core'
-export const PICTURES = 'pictures'
+export const DEFAULT_AVATAR_PATH = '/uploads/images/default_avatar.png'
+
+export const CORE_PATH = '/uploads/core/'
+export const IMAGES_PATH = '/uploads/images/'
+
+export const AWARDS_PATH = '/uploads/core/awards/'
+export const BRANDS_PATH = '/uploads/core/brands/'
+export const CARROUSEL_PATH = '/uploads/core/carrousel/'
+export const PRODUCTS_PATH = '/uploads/core/products/'
