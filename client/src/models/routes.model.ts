09b22ff58657da575159231aa1c1858f4c86cfc4
Luem <luchemma@gmail.com>
2023-05-26 15:52:03 -0300

    delete old client on proyect and add baseUrl tsconfig

---
+++ client/src/models/routes.model.ts
@@ -0,0 +1,5 @@
+export enum Routes {
+    PROFILE = 'profile',
+    HOME = '/',
+    REGISTER = 'register',
+}
--- old-client/src/components/Paypal/index.ts
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export const checkoutPaypal = async (cartBuy) => {
-	const response = await axios.post("/create-order", cartBuy);
-	return response;
-};
