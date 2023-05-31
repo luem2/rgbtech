aba8d172a1c9019b6bf3b9aa1c4914b7a4fff39d
Luem <luchemma@gmail.com>
2023-05-30 21:12:42 -0300

    add icons and barrels

---
+++ client/src/components/icons/index.ts
@@ -0,0 +1,20 @@
+export {
+    BsFillCartFill,
+    BsFillSuitHeartFill,
+    BsFillHeartFill,
+    BsHeart,
+    BsEmojiHeartEyesFill,
+    BsFillSunFill,
+    BsFillMoonFill,
+    BsSearch,
+} from 'react-icons/bs'
+export { ImCart } from 'react-icons/im'
+export {
+    FaShoppingCart,
+    FaUser,
+    FaUserAstronaut,
+    FaSearch,
+} from 'react-icons/fa'
+export { CgSpinner, CgSpinnerTwo } from 'react-icons/cg'
+export { GiAstronautHelmet } from 'react-icons/gi'
+export { MdRocketLaunch } from 'react-icons/md'
---
+++ client/src/components/index.ts
@@ -0,0 +1,3 @@
+export * from './Categories'
+export * from './Header'
+export * from './Searchbar'
